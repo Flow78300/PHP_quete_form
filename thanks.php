9142657b02c07a46412a88d82144d964ace89101
Florian Dugué <79923290+Flow78300@users.noreply.github.com>
2021-10-05 10:56:38 +0200

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,12 @@
+<?php
+            
+    
+
+            echo "Merci $_POST[user_firstname] $_POST[user_lastname] de nous avoir contacté à propos de $_POST[subject]." . "<br>";
+            echo "Un de nos conseiller vous contactera soit à l’adresse $_POST[user_mail] ou par téléphone au $_POST[user_phone_number] dans les plus brefs délais pour traiter votre demande :" . "<br>";
+            echo " $_POST[user_message].";
+            
+            
+            
+            
+?>
